f235366aba737e9b95593a2ad9fb2bdaddc7e52c
Sudarshan <sudarshanmali@siddhatech.com>
2023-12-05 15:52:51 +0530

    Added new Test Suite and Test Case

---
+++ Test Suites/TS2_Verify Successful Appointment.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS2_Verify Successful Appointment</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>aa3a1ba9-9391-4725-b5d6-cee826b15a6f</testSuiteGuid>
+   <testCaseLink>
+      <guid>5c55fcc1-c9fc-4df1-80cc-c6500d8cbee7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/TC2_Verify Successful Appointment</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
